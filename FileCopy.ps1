4157d2f03e3d7ae0ab875ddf0e2a534d637c8e54
unknown <aabdu@webserver.cosmostx.org>
2014-11-25 16:40:20 -0600

    if folder is empty hand;er

--- FileCopy.ps1
+++ FileCopy.ps1
@@ -12,7 +12,12 @@ $trg = "C:\t2\"
 function Copy-Files ([string]$source,[string]$destination){
     $srcc = Get-childitem $source
     $trgc = Get-childitem $destination
-    Compare-Object $srcc $trgc -Property Name, Length | Where-Object {$_.SideIndicator -eq "<=" } | ForEach-Object {
+    
+    if($trgc.count -ne 0)
+    {
+
+     Compare-Object $srcc $trgc -Property Name, Length | Where-Object {$_.SideIndicator -eq "<=" } | ForEach-Object {
+        
         if($_.Length -eq $null){ # this is new folder folder
             Copy-Item $($source + $_.name) $($destination + $_.name) -recurse
         }
@@ -26,6 +31,9 @@ function Copy-Files ([string]$source,[string]$destination){
             }
         }
     }
+    }
+    
+   
     Copy-Folders $source $destination
 }
 
@@ -33,10 +41,14 @@ function Copy-Folders ([string]$source,[string]$destination){
     $srcc = Get-childitem $source
     $trgc = Get-childitem $destination
 
+    if($trgc.count -ne 0)
+    {
+
     Compare-Object $srcc $trgc -Property FullName, Length,Name | Where-Object {$_.SideIndicator -eq "<=" -and $_.Length -eq $null} |ForEach-Object {
         
     if(Test-Path $($destination + $_.name)){ #existing folder
         Copy-Files $($_.Fullname+"\") $($destination + $_.Name+"\")
+    }
     }
 }
 }
