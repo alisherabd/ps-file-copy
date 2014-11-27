26fe424e1e26cd80ca351f85c60af13eced8fe2e
unknown <aabdu@webserver.cosmostx.org>
2014-11-26 20:55:42 -0600

    new version first commit

---
+++ FileCopy-v2.ps1
@@ -0,0 +1,22 @@
+﻿$src = "C:\t1\"
+$trg = "C:\t2\"
+
+function Copy-Files ([string]$source,[string]$destination){
+    $srcc = Get-childitem $source
+    $trgc = Get-childitem $destination
+
+   if($trgc.count -ne 0 ){
+     Compare-Object $srcc $trgc -Property Name, Extension, LastWriteTime,BaseName,PSIsContainer | Where-Object {$_.SideIndicator -eq "<=" } | ForEach-Object {
+        Write-Host $_
+    }
+    }
+    else
+    {
+        
+    }
+
+}
+
+
+
+Copy-Files $src $trg
